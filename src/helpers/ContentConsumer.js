04b2595117f110243db092ff14dd7f02798ace37
yemijaiyeola <yemi.jaiyeola@wearesnook.com>
2021-04-13 13:14:07 +0100

    add env vars remove console logs

--- src/helpers/ContentConsumer.js
+++ src/helpers/ContentConsumer.js
@@ -1,24 +1,21 @@
-//lets refactor this we can look at together but I have made this env var change because the url is likely to change
-const BASE_URL  = "https://admin.beta.openreferraluk.org";
-const LANDING_PAGE_URI = '/home-page';
-const MENU_URI = '/top-menus';
+
 const SUB_MENU_URI = '/sub-menus/';
 
-//const BASE_URL  = process.env.REACT_APP_BASE_URL;
-//const LANDING_PAGE_URI = process.env.REACT_APP_HOME_PAGE_URI;
-//const MENU_URI = process.env.;
+const BASE_URL  = process.env.REACT_APP_BASE_URL;
+const LANDING_PAGE_URI = process.env.REACT_APP_HOME_PAGE_URI;
+const MENU_URI = process.env.REACT_APP_MENU_URI;
 
 
 export const fetchLandingPageContent = () => {
-  //consider using nlogge
+  //consider using nlogger
   
   return fetch(BASE_URL + LANDING_PAGE_URI)
     .then(async res => { 
       if (res.status === 200) {
-        console.log("response from fetch home ", res.status);
         return await res.json();
       } else {
-        console.log("response status ", res.status)
+        //add logger here
+        console.log("response status ", res.status, BASE_URL + LANDING_PAGE_URI)
       }
     }).catch(err => { console.log(err); });
 }
@@ -31,6 +28,7 @@ export const fetchMainMenuItems = () => {
         
         return await res.json();
       } else {
+         //add logger here
         console.log("response from fetch menu ", res.status);
       }
       
@@ -38,7 +36,6 @@ export const fetchMainMenuItems = () => {
 }
 
 export const fetchSubMenuItems = subMenuId => {
-  console.log("fetch with submenuid ", subMenuId);
   return fetch(BASE_URL + SUB_MENU_URI + subMenuId)
     .then(res => res.json()).catch(err => { throw new Error("Network time out")});
 }
