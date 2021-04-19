a52ad071315c6f83c93d5fa9f8573bdf039d6fee
yemijaiyeola <yemi.jaiyeola@wearesnook.com>
2021-04-19 08:08:39 +0100

    refactor component and add key

--- src/components/htmlsection/index.js
+++ src/components/htmlsection/index.js
@@ -1,19 +1,17 @@
+import { Fragment } from 'react';
 import InjectHtml from '../home/InjectHtml';
 
 
 
 const HtmlSection = ({sections}) => {
 
-    return ( <div>
-    {sections.map((item, index) => {
-       return <>
-             <InjectHtml fix={`${index}head`} paragraphText={item.sectionHeading}/>
-             <InjectHtml fix={`${index}body`} paragraphText={item.sectionBody}/>
-         </>
-    })}
-   
-   </div>
-   
+    return ( 
+        sections.map((item, index) => {
+        return <Fragment key="itemkey">
+             <InjectHtml itemKey={`${index}head`} paragraphText={item.sectionHeading}/>
+             <InjectHtml itemKey={`${index}body`} paragraphText={item.sectionBody}/>
+         </Fragment>
+        })
         )
     }
     export default HtmlSection;
