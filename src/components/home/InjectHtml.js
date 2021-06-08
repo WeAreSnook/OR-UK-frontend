dd432587d0ae86e326ce89b94c7516b0cc2399c8
matt-porism <matthew.dalton@porism.com>
2021-06-08 20:25:40 +0100

    ORUK-32

--- src/components/home/InjectHtml.js
+++ src/components/home/InjectHtml.js
@@ -2,13 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import DOMPurify from 'dompurify';
 
+const BASE_URL = process.env.REACT_APP_BASE_URL || 'https://admin.beta.openreferraluk.org';
+
 const cleanMarkup = (dirtyUnsafe) => {
     const sanitizedParam = DOMPurify.sanitize(dirtyUnsafe);
     return sanitizedParam;
 };
 
+const correctImgPath = (html) => html.replace(/src="\/uploads\/(.*)"/mg, `src="${BASE_URL}\/uploads\/$1"`);
+
 const InjectHtml = ({ paragraphText, itemKey, sectionClassName='format' } ) => 
-    <section key={itemKey} className={sectionClassName} dangerouslySetInnerHTML={{ __html: cleanMarkup(paragraphText) }}></section>;
+    <section key={itemKey} className={sectionClassName} dangerouslySetInnerHTML={{ __html: correctImgPath(cleanMarkup(paragraphText)) }}></section>;
 
 InjectHtml.propTypes = {
     paragraphText: PropTypes.string,
