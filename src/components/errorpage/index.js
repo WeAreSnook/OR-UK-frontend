fd12f4bdfe9e3cbebbc38217642dca750d3bcc1a
matt-porism <matthew.dalton@porism.com>
2021-06-22 10:45:26 +0100

    minor change

--- src/components/errorpage/index.js
+++ src/components/errorpage/index.js
@@ -13,7 +13,7 @@ const NotFound = ({ parent }) => <main id="content" className="main-container">
                 If you pasted the web address, check you copied the entire address.
             </p>
             <p>
-                Or, you can navigate to the homepage and find the page in the website's menu.
+                Or, you can navigate to the <a href="/">homepage</a> and find the page in the website's menu.
             </p>
             <p>
                 If you're still not able to find what you need, <a href="mailto:hello@openreferraluk.org">contact us</a> for help.
