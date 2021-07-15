85d3b4bc3eb25b4c61846d47df5b6555c2ef0ecd
matt-porism <matthew.dalton@porism.com>
2021-07-15 20:47:28 +0100

    changes to registration pages

--- src/components/genericcontentpage/RegisterContent.js
+++ src/components/genericcontentpage/RegisterContent.js
@@ -1,82 +1,142 @@
 import { covertHeadingToIdentifier, HtmlSection } from '../htmlsection';
+import { useState } from 'react';
 import GenericContent from './GenericContent';
 import InjectHtml from '../home/InjectHtml';
+import 'formdata-polyfill';
 
-const KeepInformedSection = ({ section, index }) => <HtmlSection key={section.id} index={index} section={section}>
-    <form className="form-section">
-        <h3>Sign up to our news letter</h3>
-        <label>Email address</label>
-        <div className="input-with-button">
-            <input type="text" id="email-address" name="email-address" />
-            <button type="submit" className="button button-primary">Subscribe</button>
+const apiUrl = 'https://validator.openreferraluk.org/api';
+
+const onSubmitSetResponse = async (e, setResponse) => {
+    e.preventDefault();
+
+    const form = e.target;
+    const data = new URLSearchParams(new FormData(form));
+
+    fetch(form.action, {
+        method: form.method,
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        //credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        //redirect: 'follow', // manual, *follow, error
+        //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: data
+    })
+        .then(response => setResponse(response.json()))
+        .catch(error => {
+            console.log(error);
+            setResponse({ success: false });
+        });
+};
+
+const ResponseMessage = ({ response }) => {
+    const { success, error, errors } = response;
+
+    if (success !== true && success !== false)
+        return null;
+
+    if (success)
+        return <div className="alert alert-success mt-1rem">Registered successfully</div>;
+
+    if (errors)
+        return <div className="alert alert-danger mt-1rem"><ul>{errors.map(e => <li>{e}</li>)}</ul></div>;
+
+    if (error)
+        return <div className="alert alert-danger mt-1rem">{error}</div>;
+        
+    return <div className="alert alert-danger mt-1rem">Registration unsuccessful</div>;
+};
+
+const KeepInformedSection = ({ section, index }) => {
+    const [response, setResponse] = useState({});
+    const onSubmit = e => onSubmitSetResponse(e, setResponse);
+
+    return <HtmlSection key={section.id} index={index} section={section}>
+        <form className="form-section" action={`${apiUrl}/registerUser`} method="post" onSubmit={onSubmit}>
+            <h3>Sign up to our news letter</h3>
+            <label>Email address</label>
+            <div className="input-with-button">
+                <input type="text" id="email-address" name="email_address" />
+                <button type="submit" className="button button-primary">Subscribe</button>
+            </div>
+            <ResponseMessage response={response} />
+        </form>
+    </HtmlSection>;
+};
+
+const RegisterYourOrganisation = ({ section, index }) => {    
+    const [response, setResponse] = useState({});
+    const onSubmit = e => onSubmitSetResponse(e, setResponse);
+    
+    return <form className="form-section" action={`${apiUrl}/registerOrganisation`} method="post" onSubmit={onSubmit}>
+        <h3>{section.sectionHeading}</h3>
+        <InjectHtml itemKey={`${index}body`} paragraphText={section.sectionBody} />
+
+        <div className="form-item">
+            <label htmlFor="organisation-name">Organisation name</label>
+            <input type="text" id="organisation-name" name="organisation_name" />
         </div>
-    </form>
-</HtmlSection>;
 
-const RegisterYourOrganisation = ({ section, index }) => <form className="form-section">
-    <h3>{section.sectionHeading}</h3>
-    <InjectHtml itemKey={`${index}body`} paragraphText={section.sectionBody} />
+        <div className="form-item">
+            <label htmlFor="organisation-type">Organisation type</label>
+            <input type="text" id="organisation-type" name="organisation_type" list="organisation-types" />
+            <datalist id="organisation-types">
+                <option value="Commissioning a directory of services" />
+                <option value="Supplying directory software and API" />
+                <option value="Consuming an Open Referral compliant API" />
+                <option value="Maintaining service data" />
+            </datalist>
+        </div>
 
-    <div className="form-item">
-        <label htmlFor="organisation-name">Organisation name</label>
-        <input type="text" id="organisation-name" name="organisation-name" />
-    </div>
-
-    <div className="form-item">
-        <label htmlFor="organisation-type">Organisation type</label>
-        <input type="text" id="organisation-type" name="organisation-type" list="organisation-types" />
-        <datalist id="organisation-types">
-            <option value="Commissioning a directory of services" />
-            <option value="Supplying directory software and API" />
-            <option value="Consuming an Open Referral compliant API" />
-            <option value="Maintaining service data" />
-        </datalist>
-    </div>
-
-    <div className="form-item">
-        <label>Organisation adoptation stage</label>
-        <div className="check-list">
-            <div className="check-item">
-                <input type="radio" id="adoptation-considering" name="adoptation" value="Considering" /> <label htmlFor="adoptation-considering">Considering</label>
-            </div>
-            <div className="check-item">
-                <input type="radio" id="adoptation-adopting" name="adoptation" value="Adopting" /> <label htmlFor="adoptation-adopting">Adopting</label>
-            </div>
-            <div className="check-item">
-                <input type="radio" id="adoptation-adopted" name="adoptation" value="Adopted" /> <label htmlFor="adoptation-adopted">Adopted</label>
+        <div className="form-item">
+            <label>Organisation adoptation stage</label>
+            <div className="check-list">
+                <div className="check-item">
+                    <input type="radio" id="adoptation-considering" name="adoptation_stage" value="Considering" /> <label htmlFor="adoptation-considering">Considering</label>
+                </div>
+                <div className="check-item">
+                    <input type="radio" id="adoptation-adopting" name="adoptation_stage" value="Adopting" /> <label htmlFor="adoptation-adopting">Adopting</label>
+                </div>
+                <div className="check-item">
+                    <input type="radio" id="adoptation-adopted" name="adoptation_stage" value="Adopted" /> <label htmlFor="adoptation-adopted">Adopted</label>
+                </div>
             </div>
         </div>
-    </div>
 
-    <div className="form-item">
-        <label htmlFor="web-link">
-            Web link URL (optional)
-            <small>Provide a link for people to read about your adoptation or organisation.</small>
-        </label>
-        <input type="text" id="web-link" name="web-link" />
-    </div>
+        <div className="form-item">
+            <label htmlFor="url">
+                Web link URL (optional)
+                <small>Provide a link for people to read about your adoptation or organisation.</small>
+            </label>
+            <input type="text" id="url" name="url" />
+        </div>
 
-    <div className="form-item">
-        <label htmlFor="private-email-address">
-            Private email address
-            <small>So that we can follow-up with you to verify your organisation. Will not be shared.</small>
-        </label>
-        <input type="text" id="private-email-address" name="private-email-address" />
-    </div>
+        <div className="form-item">
+            <label htmlFor="private-email-address">
+                Private email address
+                <small>So that we can follow-up with you to verify your organisation. Will not be shared.</small>
+            </label>
+            <input type="text" id="private-email-address" name="private_email_address" />
+        </div>
 
-    <div className="form-item">
-        <label htmlFor="public-email-address">
-            Public email address (optional)
-            <small>Will be sared alongside your organisation on our Who's using page.</small>
-        </label>
-        <input type="text" id="public-email-address" name="public-email-address" />
-    </div>
+        <div className="form-item">
+            <label htmlFor="public-email-address">
+                Public email address (optional)
+                <small>Will be sared alongside your organisation on our Who's using page.</small>
+            </label>
+            <input type="text" id="public-email-address" name="public_email_address" />
+        </div>
 
-    <div className="form-item">
-        <br />
-        <button type="submit" className="button button-primary">Register organisation</button>
-    </div>
-</form>;
+        <div className="form-item">
+            <br />
+            <button type="submit" className="button button-primary">Register organisation</button>
+        </div>
+        
+        <ResponseMessage response={response} />
+    </form>;
+};
 
 const RegisterSections = ({ sections }) => sections.map((section, index) => {
     const identifier = covertHeadingToIdentifier(section.sectionHeading);
