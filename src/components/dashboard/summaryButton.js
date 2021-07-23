452046a702e20e4b4b1532ae5530464cf8b7c81c
adamgparsons <adam@adamparsons.me>
2021-07-23 10:16:32 +0100

    added "close" wording from ticket #239 to ensure spacing is correct

--- src/components/dashboard/summaryButton.js
+++ src/components/dashboard/summaryButton.js
@@ -1,21 +1,28 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const SummaryButton = ({ summary }) => {
+  const [isOpen, setIsOpen] = useState(false);
 
-    const [isOpen, setIsOpen] = useState(false);
+  if (!summary) return <span>-</span>;
 
-    if (!summary)
-        return <span>-</span>;
-
-    return <>
-        <button className='button-link' onClick={() => setIsOpen(true)}>Read</button>
-        <div className={`modal-background ${isOpen ? 'visible' : ''}`}>
-            <div className='modal'>
-                <button className='close-button button-link' onClick={() => setIsOpen(false)}>×</button>
-                {summary}
-            </div>
+  return (
+    <>
+      <button className="button-link" onClick={() => setIsOpen(true)}>
+        Read
+      </button>
+      <div className={`modal-background ${isOpen ? "visible" : ""}`}>
+        <div className="modal">
+          <button
+            className="close-button button-link"
+            onClick={() => setIsOpen(false)}
+          >
+            close
+          </button>
+          {summary}
         </div>
-    </>;
+      </div>
+    </>
+  );
 };
 
 export default SummaryButton;
